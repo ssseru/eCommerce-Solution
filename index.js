e1e3d122b1515d3262b39bdedd9677fcbf6f2b4b
Sai Sagar Seru <saisseru@gmail.com>
2021-07-23 12:38:16 +0530

    updated index.js file to use routes and models

--- index.js
+++ index.js
@@ -6,7 +6,32 @@ const path = require("path");
 
 require("dotenv").config();
 
+const users = require("./routes/api/userRouter");
+
 const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+const db = process.env.mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+require("./models/userModel");
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
