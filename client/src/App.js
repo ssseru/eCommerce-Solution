ec244f95d2a169be7e6103736081f2113c8f7317
Sai Sagar Seru <saisseru@gmail.com>
2021-07-24 12:44:28 +0530

    making components for home screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
-
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import HomeComponent from "./components/HomeComponent";
 function App() {
-  return <div className="App">H</div>;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={HomeComponent} />
+      {/* <Route path="/signin" component={SignInComponent} /> */}
+      {/* <Route path="/register" component={RegisterComponent} /> */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
