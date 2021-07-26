6dce491a579479df52942e641d322b128e651f3f
Sai Sagar Seru <saisseru@gmail.com>
2021-07-26 17:09:42 +0530

    style signin part 1

--- client/src/components/SignInComponent.js
+++ client/src/components/SignInComponent.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { signin } from "../actions/userAction";
+import {
+  Container,
+  Input,
+  Label,
+  Form,
+  Col,
+  Button,
+  FormGroup,
+} from "reactstrap";
 // import LoadingBox from '../components/LoadingBox';
 // import MessageBox from '../components/MessageBox';
 
@@ -37,55 +46,42 @@ function SignInComponent(props) {
   }, [userInfo, redirect, props.history]);
 
   return (
-    <div>
-      <form className="form" onSubmit={submitHandler}>
-        <div>
-          <h1>Sign In</h1>
-          {/* {
-                        loading && 
-                        <LoadingBox />
-                    }
-                    {
-                        error && 
-                        <MessageBox variant="danger">{error}</MessageBox>
-                    } */}
-        </div>
-        <div>
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter email"
-            value={email}
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter password"
-            value={password}
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label />
-          <button className="primary" type="submit">
-            Sign In
-          </button>
-        </div>
-        <div>
-          <label />
-          <div>
-            New customer? <Link to={`/register`}>Create your account</Link>
-          </div>
-        </div>
-      </form>
-    </div>
+    <Container>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <FormGroup row>
+          <Label for="email" md={2}>
+            Email:{" "}
+          </Label>
+          <Col md={10}>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Enter email senpai"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Col>
+        </FormGroup>
+        <Label for="password">Password: </Label>
+        <Input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button type="submit">Sign In</Button>
+        <br />
+      </Form>
+      <div>
+        New customer? <Link to={`/register`}>Create your account</Link>
+      </div>
+    </Container>
   );
 }
 
